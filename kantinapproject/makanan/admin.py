be846b65d91908bf221706d7de911552e0cd59ea
Kzdevelop25 <tlod9906@gmail.com>
2022-12-17 15:00:03 +0800

    Project Kantin

---
+++ kantinapproject/makanan/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Kantin
+from .models import Makanan
+
+
+# Register your models here.
+admin.site.register(Kantin)
+admin.site.register(Makanan)
+
